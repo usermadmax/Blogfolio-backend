767dca394b2c5483c9031ce5454b6ac430149d6f
usermadmax <ganeshaenagandula2005@gmail.com>
2025-06-29 16:07:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app=require('./app')
 const config=require('./utils/config')
 const logger=require('./utils/loggers')
 mongoose.connect(config.MONGODB_URI)
-.then(()=>{
-    logger.info('Connected to MongoDB')
-})
-.catch((error)=>{
-    logger.error('Error connecting to MongoDB:', error.message)
-})
+  .then(() => {
+    logger.info('Connected to MongoDB');
 
-const PORT=config.PORT
-app.listen(PORT,()=>{
-    logger.info(`Server running on port ${PORT}`)
-})
+    const PORT = config.PORT;
+    app.listen(PORT, () => {
+      logger.info(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB:', error.message);
+  });
